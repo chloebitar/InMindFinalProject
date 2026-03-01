f9f152f18169e7fa6928fc85e794cd49395db98c
chloebitar <chloebitar32@gmail.com>
2026-03-01 21:20:01 +0200

    added a currency converter

--- src/app/core/Layout/header/header.ts
+++ src/app/core/Layout/header/header.ts
@@ -1,6 +1,7 @@
 import { Component,inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../../shared/services/cart-service';
+import { CurrencyService } from '../../../shared/services/currency-service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,9 @@ import { CartService } from '../../../shared/services/cart-service';
 })
 export class Header {
   cart = inject(CartService);
+  currencyService = inject(CurrencyService);
+
+  toggleCurrency() {
+    this.currencyService.toggle();
+  }
 }
