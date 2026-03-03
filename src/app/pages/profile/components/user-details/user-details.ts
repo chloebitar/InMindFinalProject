b87ef21b27d427d8b84c3f91d507039d8d48cdb0
chloebitar <chloebitar32@gmail.com>
2026-03-04 01:44:49 +0200

    added a tab switcher in profile page

---
+++ src/app/pages/profile/components/user-details/user-details.ts
@@ -0,0 +1,60 @@
+import { Component, computed, effect, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../../../core/auth/auth-services/auth-service';
+import { IUser } from '../../../../core/auth/auth-interfaces/user';
+@Component({
+  selector: 'app-user-details',
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-details.html',
+  styleUrl: './user-details.scss',
+})
+export class UserDetails {
+  private auth = inject(AuthService);
+  private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+
+  user = this.auth.user;
+
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', Validators.required],
+  });
+
+  constructor() {
+    effect(() => {
+      const u = this.user();
+      if (!u) return;
+
+      this.form.reset(
+        {
+          firstName: u.firstName ?? '',
+          lastName: u.lastName ?? '',
+          email: u.email ?? '',
+          username: u.username ?? '',
+        },
+        { emitEvent: false },
+      );
+    });
+  }
+
+  save() {
+    if (this.form.invalid) return;
+
+    const values = this.form.value;
+
+    const fd = new FormData();
+    fd.append('firstName', values.firstName!);
+    fd.append('lastName', values.lastName!);
+    fd.append('email', values.email!);
+    fd.append('username', values.username!);
+    this.http
+      .patch<IUser>('https://melaine-palaeobiologic-savourily.ngrok-free.dev/api/user', fd)
+      .subscribe((updated) => {
+        this.auth.setUser(updated);
+      });
+  }
+}
--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,21 +1,18 @@
 import { Component, computed, effect, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AuthService } from '../../core/auth/auth-services/auth-service';
-import { IUser } from '../../core/auth/auth-interfaces/user';
-
+import { UserDetails } from './components/user-details/user-details';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatTabsModule, UserDetails],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
   private auth = inject(AuthService);
-  private fb = inject(FormBuilder);
-  private http = inject(HttpClient);
 
   user = this.auth.user;
 
@@ -26,45 +23,4 @@ export class Profile {
     const b = (u.lastName?.[0] ?? '').toUpperCase();
     return a + b || (u.username?.[0]?.toUpperCase() ?? '??');
   });
-
-  form = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    username: ['', Validators.required],
-  });
-
-  constructor() {
-    effect(() => {
-      const u = this.user();
-      if (!u) return;
-
-      this.form.reset(
-        {
-          firstName: u.firstName ?? '',
-          lastName: u.lastName ?? '',
-          email: u.email ?? '',
-          username: u.username ?? '',
-        },
-        { emitEvent: false },
-      );
-    });
-  }
-
-  save() {
-    if (this.form.invalid) return;
-
-    const values = this.form.value;
-
-    const fd = new FormData();
-    fd.append('firstName', values.firstName!);
-    fd.append('lastName', values.lastName!);
-    fd.append('email', values.email!);
-    fd.append('username', values.username!);
-    this.http
-      .patch<IUser>('https://melaine-palaeobiologic-savourily.ngrok-free.dev/api/user', fd)
-      .subscribe((updated) => {
-        this.auth.setUser(updated);
-      });
-  }
 }
