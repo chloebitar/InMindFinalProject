c8a34be32e44ae43a9942c33e5824be2e0a44219
chloebitar <chloebitar32@gmail.com>
2026-03-04 15:47:16 +0200

    added email using emailJS

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  emailjs: {
+    serviceId: 'service_2a67rhl',
+    templateId: 'template_n1m5wt9',
+    publicKey: '0ucxzzVqeKd1gRsRA',
+  },
+};
